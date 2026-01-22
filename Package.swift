cbf8c37bce4f3777653965e8b7798660b698cfed
Ajinkya Chhatre <ajinkya.chhatre@rakuten.com>
2026-01-22 13:17:00 +0530

    Add OneClick binary SPM package

---
+++ Package.swift
@@ -0,0 +1,19 @@
+import PackageDescription
+
+let package = Package(
+    name: "OneClick",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "OneClick",
+            targets: ["OneClick"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "OneClick",
+            url: "https://github.com/Rakuten-MTSD-PAIS/oneclick-ios-spm/releases/download/1.0.0/OneClick.xcframework.zip",
+            checksum: "029919573abbc8c6cccaee3ec0f414fe3f1f205259d9abcadf7793633bb22d8b"
+        )
+    ]
+)
