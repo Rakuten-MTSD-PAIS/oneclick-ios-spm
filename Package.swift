2a00f552e8269f2c65c4196038eb4a99d6e74c5e
Ajinkya Chhatre <ajinkya.chhatre@rakuten.com>
2026-01-23 13:09:32 +0530

    Update OneClick binary to 2.600.0004

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OneClick",
-            url: "https://github.com/Rakuten-MTSD-PAIS/oneclick-ios-spm/releases/download/1.0.0/OneClick.xcframework.zip",
+            url: "https://github.com/Rakuten-MTSD-PAIS/oneclick-ios-spm/releases/download/2.600.0004/OneClick.xcframework.zip",
             checksum: "029919573abbc8c6cccaee3ec0f414fe3f1f205259d9abcadf7793633bb22d8b"
         )
     ]
